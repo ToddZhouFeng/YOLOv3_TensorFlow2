141e691c765be298305c3b22d6c9e4c77e2bdd93
ToddZhou <todd310378072@outlook.com>
2020-09-30 14:06:00 +0800

    yolo-fastest

--- configuration.py
+++ configuration.py
@@ -5,15 +5,15 @@ load_weights_before_training = False
 load_weights_from_epoch = 10
 
 # input image
-IMAGE_HEIGHT = 416
-IMAGE_WIDTH = 416
+IMAGE_HEIGHT = 224
+IMAGE_WIDTH = 224
 CHANNELS = 3
 
 # Dataset
-CATEGORY_NUM = 80
-ANCHOR_NUM_EACH_SCALE = 3
+CATEGORY_NUM = 20
+ANCHOR_NUM_EACH_SCALE = 2
 COCO_ANCHORS = [[116, 90], [156, 198], [373, 326], [30, 61], [62, 45], [59, 119], [10, 13], [16, 30], [33, 23]]
-COCO_ANCHOR_INDEX = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+COCO_ANCHOR_INDEX = [[0, 1, 2], [3, 4, 5]] #, [6, 7, 8]]
 SCALE_SIZE = [13, 26, 52]
 
 use_dataset = "pascal_voc"      # "custom", "pascal_voc", "coco"
